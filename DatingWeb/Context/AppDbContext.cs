ee83b1a5c4527f55434897796d74fbd2ad44e105
Maruf Berdiyev <93835738+fuga02@users.noreply.github.com>
2023-10-24 17:56:51 +0800

    DbContext with nuget packeges were added

---
+++ DatingWeb/Context/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using DatingWeb.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingWeb.Context;
+
+public class AppDbContext:DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        base.OnConfiguring(optionsBuilder);
+    }
+
+    public DbSet<User> Users { get; set; }
+}
