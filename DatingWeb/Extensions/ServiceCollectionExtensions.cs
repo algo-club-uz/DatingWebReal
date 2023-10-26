78f78e39751ff652332c716bc467fcd97c8dc850
Maruf Berdiyev <93835738+fuga02@users.noreply.github.com>
2023-10-26 15:53:11 +0800

    ok

---
+++ DatingWeb/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,43 @@
+﻿using DatingWeb.Managers;
+using DatingWeb.Options;
+using DatingWeb.Providers;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+
+namespace DatingWeb.Extensions;
+
+public static  class ServiceCollectionExtensions
+{
+    private static void AddJwt(this IServiceCollection services, IConfiguration configuration)
+    {
+        var section = configuration.GetSection(nameof(JwtOptions));
+        services.Configure<JwtOptions>(section);
+        JwtOptions jwtOptions = section.Get<JwtOptions>()!;
+        services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options =>
+            {
+                var signingKey = System.Text.Encoding.UTF32.GetBytes(jwtOptions.SigningKey);
+                options.TokenValidationParameters = new TokenValidationParameters()
+                {
+                    ValidIssuer = jwtOptions.ValidIssuer,
+                    ValidAudience = jwtOptions.ValidAudience,
+                    ValidateAudience = true,
+                    ValidateIssuer = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(signingKey),
+                    ValidateIssuerSigningKey = true,
+                    ValidateLifetime = true,
+                    ClockSkew = TimeSpan.Zero
+                };
+            });
+    }
+
+    public static void AddIdentity(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddJwt(configuration);
+        services.AddScoped<JwtTokenManager>();
+        services.AddScoped<UserManager>();
+        services.AddHttpContextAccessor();
+        services.AddScoped<UserProvider>();
+    }
+
+}
