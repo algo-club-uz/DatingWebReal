f6b957d830301aa1e0a73ea8f1c6e6b5d44b982f
Maruf Berdiyev <93835738+fuga02@users.noreply.github.com>
2023-10-30 16:33:36 +0800

    Chat Repository was added

---
+++ DatingWeb/Repositories/Interfaces/IChatRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace DatingWeb.Repositories.Interfaces;
+
+public interface IChatRepository
+{
+    Task StartOrContinueChat(Guid currentUserId, Guid toUserId);
+    Task SendMessage(Guid fromUserId, Guid toUserId, string message);
+    Task DeleteMessage(Guid fromUserId, Guid toUserId, Guid messageId);
+}
