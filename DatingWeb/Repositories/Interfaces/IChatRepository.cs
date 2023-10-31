543cf180b2d86f80ae3494db6bcc1393caf43729
Maruf Berdiyev <93835738+fuga02@users.noreply.github.com>
2023-11-01 01:35:00 +0800

    logic which was missed was added

--- DatingWeb/Repositories/Interfaces/IChatRepository.cs
+++ DatingWeb/Repositories/Interfaces/IChatRepository.cs
@@ -4,6 +4,8 @@ namespace DatingWeb.Repositories.Interfaces;
 
 public interface IChatRepository
 {
+    Task<List<Chat>> GetChats(Guid currentUserId);
+    Task<Chat> GetChat(Guid currentUserId,Guid userId);
     Task<Chat> StartOrContinueChat(Guid currentUserId, Guid toUserId);
     Task<Message> SendMessage(Guid fromUserId, Guid toUserId, string message);
     Task DeleteMessage( Guid messageId);
