232b38d76034dbe72f47fb6bb5d5ac8f6b86747e
Maruf Berdiyev <93835738+fuga02@users.noreply.github.com>
2023-11-04 03:48:41 +0800

    Register logic was added

--- DatingFront/Program.cs
+++ DatingFront/Program.cs
@@ -1,4 +1,5 @@
 using DatingFront;
+using DatingFront.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -6,6 +7,8 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped<RequestService>();
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5198/api") });
 
 await builder.Build().RunAsync();
---
+++ DatingFront/Services/RequestService.cs
@@ -0,0 +1,21 @@
+﻿using System.Net.Http.Json;
+using CommonFiles.Models;
+
+namespace DatingFront.Services;
+
+public class RequestService
+{
+    private readonly HttpClient _httpClient;
+
+    public RequestService(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    public async Task Register(CreateUserModel model)
+    {
+        var request = new HttpRequestMessage(HttpMethod.Post, "/Accounts/register");
+        request.Content = JsonContent.Create(model);
+        await _httpClient.SendAsync(request);
+    }
+}
