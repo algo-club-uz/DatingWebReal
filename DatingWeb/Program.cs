d06eaaf8d9a7a5d8459a7fa1a4b920d0e87b0634
Maruf Berdiyev <93835738+fuga02@users.noreply.github.com>
2023-11-09 23:29:37 +0800

    ok

--- DatingWeb/Program.cs
+++ DatingWeb/Program.cs
@@ -4,7 +4,6 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddCors();
 // Add services to the container.
 
 builder.Services.AddControllers();
